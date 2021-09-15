776f38b631d55abb4613a958eec689714012a001
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-09-15 17:01:28 -0300

    Added sunlight

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -13,7 +13,8 @@ export function Earth(props) {
   
   return (
     <>
-      <ambientLight intensity={1} />
+      {/* <ambientLight intensity={1} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2} />
       <Stars
         radius={300}
         depth={60}
@@ -35,7 +36,12 @@ export function Earth(props) {
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
         <OrbitControls
           enableZoom={true}
           enablePan={true}
