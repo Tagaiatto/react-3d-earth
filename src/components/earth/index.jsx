53d6d28917d3d429ad9ee79e640315b0e3dfff65
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-09-15 18:50:07 -0300

    Added some night lights

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,16 +1,29 @@
+import { useRef } from 'react';
 import * as THREE from 'three';
-import { useLoader } from '@react-three/fiber';
+import { useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 import { OrbitControls, Stars } from '@react-three/drei';
 
 import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg';
+import EarthNightMap from '../../assets/textures/8k_earth_nightmap.jpg';
 import EarthNormalMap from '../../assets/textures/8k_earth_normal_map.jpg';
 import EarthSpecularMap from '../../assets/textures/8k_earth_specular_map.jpg';
 import EarthCloudsMap from '../../assets/textures/8k_earth_clouds.jpg';
 
 export function Earth(props) {
-  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap])
+  const [colorMap, nightMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNightMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap])
   
+  const earthRef = useRef();
+  const nightRef = useRef();
+  const cloudsRef = useRef();
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+
+    earthRef.current.rotation.y = elapsedTime / 6;
+    nightRef.current.rotation.y = elapsedTime / 6;
+    cloudsRef.current.rotation.y = elapsedTime / 6;
+  })
+
   return (
     <>
       {/* <ambientLight intensity={1} /> */}
@@ -23,7 +36,7 @@ export function Earth(props) {
         saturation={0}
         fade={true}
       />
-      <mesh>
+      <mesh ref={cloudsRef}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -33,7 +46,16 @@ export function Earth(props) {
           side={THREE.DoubleSide}
         />
       </mesh>
-      <mesh>
+      <mesh ref={nightRef}>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshBasicMaterial
+          map={nightMap}
+          depthWrite={true}
+          transparent={true}
+          opacity={0.2}
+        />
+      </mesh>
+      <mesh ref={earthRef}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial
